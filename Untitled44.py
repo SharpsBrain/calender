004bcef763208ba62cedbc26f3a08c103c5f74f5
SharpsBrain <109470818+SharpsBrain@users.noreply.github.com>
2022-09-20 10:55:01 +0500

    Add files via upload

---
+++ Untitled44.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# First import the calendar module  
+import calendar  
+# ask of month and year  
+yy = int(input("Enter year: "))  
+mm = int(input("Enter month: "))  
+# display the calendar  
+print(calendar.month(yy,mm))  
+
+
+# In[ ]:
+
+
+
+
